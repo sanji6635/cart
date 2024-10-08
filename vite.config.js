0d21860ca52f4d2273f6650a1001fe7ed83a4034
sanji6635 <hetiveg499@gmail.com>
2024-10-08 10:38:22 +0000

    trying

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 4200,
   },
-  plugins: [react(), vercel()],
+  plugins: [react()],
   base: "/", // Replace <repo-name> with your GitHub repository name
 });
